1fe5b3f67ab4f81b2e183a656587eb0362aed477
Jake VanderPlas <jakevdp@gmail.com>
2018-02-03 10:43:31 -0800

    More tests of BaseObject

--- altair/schema/tests/test_base.py
+++ altair/schema/tests/test_base.py
@@ -28,7 +28,7 @@ class Foo(BaseObject):
         'definitions': Derived._json_schema['definitions']
     }
 
-    def __init__(self, d='', **kwds):
+    def __init__(self, d=Undefined, **kwds):
         kwds['d'] = d
         super(Foo, self).__init__(**kwds)
 
@@ -37,9 +37,13 @@ def test_schema_cases():
     assert Derived(a=4, b='yo').to_dict() == {'a': 4, 'b': 'yo'}
     assert Derived(a=4, c={'d': 'hey'}).to_dict() == {'a': 4, 'c': {'d': 'hey'}}
     assert Derived(a=4, b='5', c=Foo('val')).to_dict() == {'a': 4, 'b': '5', 'c': {'d': 'val'}}
+    assert Foo(d='hello', f=4).to_dict() == {'d': 'hello', 'f': 4}
+
+    assert Derived().to_dict() == {}
+    assert Foo().to_dict() == {}
 
     with pytest.raises(jsonschema.ValidationError):
-        # no additional properties
+        # no additional properties allowed
         Derived(foo='bar').to_dict()
 
     with pytest.raises(jsonschema.ValidationError):
